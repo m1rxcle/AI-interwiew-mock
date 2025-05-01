d366bcdb950c3785f54a0a230e0c86dbc77e68c3
Denis M1rxcle <noonebesideu@gmail.com>
2025-05-02 01:39:37 +0200

    setup a firebase settings

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app"
+import { getAuth } from "firebase-admin/auth"
+import { getFirestore } from "firebase-admin/firestore"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCNm0oIQg0Y62x4HPj4gOUqE02uvWk9JkI",
+	authDomain: "prepwise-281e6.firebaseapp.com",
+	projectId: "prepwise-281e6",
+	storageBucket: "prepwise-281e6.firebasestorage.app",
+	messagingSenderId: "427529532411",
+	appId: "1:427529532411:web:745877af43e26fb1a3d15e",
+	measurementId: "G-K8JCYQLWT7",
+}
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
