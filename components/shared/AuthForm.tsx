52d2978c65dffb49e8c872a94bba6eaef7b7a2a5
Denis M1rxcle <noonebesideu@gmail.com>
2025-05-02 02:39:30 +0200

    implement auth and storage

--- components/shared/AuthForm.tsx
+++ components/shared/AuthForm.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/firebase/client"
+
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -11,6 +14,7 @@ import Link from "next/link"
 import { toast } from "sonner"
 import FormField from "./FormField"
 import { useRouter } from "next/navigation"
+import { signIn, singUp } from "@/lib/actions/auth.action"
 
 const authFormSchema = (type: FormType) => {
 	return z.object({
@@ -34,12 +38,42 @@ const AuthForm = ({ type }: { type: FormType }) => {
 		},
 	})
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
+	async function onSubmit(values: z.infer<typeof formSchema>) {
 		try {
 			if (type === "sign-up") {
+				const { name, email, password } = values
+
+				const userCreadentials = await createUserWithEmailAndPassword(auth, email, password)
+
+				const result = await singUp({
+					uid: userCreadentials.user.uid,
+					name: name!,
+					password,
+					email,
+				})
+
+				if (!result?.success) {
+					toast.error(result?.message)
+					return
+				}
+
 				toast.success("Account created successfully. Please sign in.")
 				router.push("/sign-in")
 			} else {
+				const { email, password } = values
+
+				const userCredential = await signInWithEmailAndPassword(auth, email, password)
+				const idToken = await userCredential.user.getIdToken()
+
+				if (!idToken) {
+					toast.error("Failed to sign in. Please try again.")
+					return
+				}
+				await signIn({
+					email,
+					idToken,
+				})
+
 				toast.success("Sign-in successful.")
 				router.push("/")
 			}
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApp, getApps } from "firebase/app"
-import { getAuth } from "firebase-admin/auth"
-import { getFirestore } from "firebase-admin/firestore"
+import { getAuth } from "@firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCNm0oIQg0Y62x4HPj4gOUqE02uvWk9JkI",
